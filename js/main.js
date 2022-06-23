9b67f451af5dc83c34d1987014cda6b2854946d1
bigeppo <carlog91cast@gmail.com>
2022-06-23 17:36:49 +0200

    add eventlistener on btn-play

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 // recupero il parent mediante id del button nell'html
 
-const play = document.getElementById('btn-genera');
- 
+
+function btnPlay() {
+    const play = document.getElementById('btn-genera');
+    play.addEventListener("click");
+};
 
 // genero la griglia con gli elementi dentro cosi poi da inserila nell'eventlistener tramite funzione
 
 function createGridContainer(grid, className) {
     // griglia vuota
-    grid.innerHTML= "";
-    
+    grid.innerHTML = "";
+
     // creo gli elementi con un ciclo for
 
     for (let i = 0; i <= 100; index++) {
-        let gridContainer = 
+        let gridContainer = gridSquare (className, i);
+        gridContainer.btnPlay('click');
         
-    }
+    };
 }
 
 
@@ -22,5 +26,7 @@ function createGridContainer(grid, className) {
 
 function createSquare(className, i) {
     let gridSquare = document.createElement('div')
-}
+    gridSquare.classList.add('square-grid')
+    return gridSquare;
+};
 
