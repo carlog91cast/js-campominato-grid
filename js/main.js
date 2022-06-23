0cfafeba9c61fe0e9cead8fe97cfa1020dbe94b7
bigeppo <carlog91cast@gmail.com>
2022-06-23 17:14:53 +0200

    add function create grid

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// recupero il parent mediante id del button nell'html
+
+const play = document.getElementById('btn-genera');
+ 
+
+// genero la griglia con gli elementi dentro cosi poi da inserila nell'eventlistener tramite funzione
+
+function createGridContainer(grid, className) {
+    // griglia vuota
+    grid.innerHTML= "";
+    
+    // creo gli elementi con un ciclo for
+
+    for (let i = 0; i <= 100; index++) {
+        let gridContainer = 
+        
+    }
+}
