703e8f532804c2e28c2ba07c3ff55fe4708792e3
bigeppo <carlog91cast@gmail.com>
2022-06-24 11:47:28 +0200

    add corro on line 29

--- js/main.js
+++ js/main.js
@@ -3,30 +3,33 @@ const gridContainer = document.getElementById('mine-camp');
 
 // creo i songoli quadrati
 playBtn.addEventListener('click', function () {
+    gridContainer.innerHTML = "";
     for (let i = 0; i <= 100; i++) {
-        // svuoto la griglia
-        gridContainer.innerHTML = "";
+
         // inserisco la funzione che crea i quadrati
-        insSquare = createSquares();
+        inSquare = createSquares();
+        // inserisco la funzione nel button
+
         // inserisco i quadrati nell'html con append
-        insSquare.innerHTML = i;
-        gridContainer.append(insSquare);
+        inSquare.innerHTML = i;
+        gridContainer.append(inSquare);
 
 
     }
+
 });
 
 
 // in una fuzione creo i singoli quadrati
 
 function createSquares() {
+
     const square = document.createElement('div')
     square.classList.add('square-grid');
-
-    // inserisco la funzione nel button
     square.addEventListener('click', function () {
         square.classList.toggle('active');
     });
+
 
     return square;
 
